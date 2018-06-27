f514fb327ec234601841a28e15def560fca0a05e
Michaela Kubišová <kubiska18@gmail.com>
2018-06-27 13:52:25 +0200

    spring demo annotations continue

---
+++ spring-demo-annotations/src/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,18 @@
+package springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Coach tennisCoach = context.getBean("tennisCoach", Coach.class);
+		
+		System.out.println(tennisCoach.getDailyWorkout());
+		System.out.println(tennisCoach.getDailyFortune());
+		
+		context.close();
+	}
+
+}
---
+++ spring-demo-annotations/src/springdemo/TennisCoach.java
@@ -0,0 +1,28 @@
+package springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+ 
+	private FortuneService fortuneService;
+		
+	public TennisCoach() {}
+
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley.";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
