052b37fbe7e7eacf8e8365d170ce41e91e1bbb54
Michaela Kubisova <kubiska18@gmail.com>
2018-07-11 13:10:31 +0200

    sping annotations demo done

---
+++ spring-demo-annotations/src/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,18 @@
+package springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+		
+		Coach tennisCoach = context.getBean("tennisCoach", Coach.class);
+		
+		System.out.println(tennisCoach.getDailyWorkout());
+		System.out.println(tennisCoach.getDailyFortune());
+		
+		context.close();
+	}
+
+}
---
+++ spring-demo-annotations/src/springdemo/SportConfig.java
@@ -0,0 +1,22 @@
+package springdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:sport.properties")
+public class SportConfig {
+	//define bean for our sad fortune service
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	//define bean for our swim coach AND inject dependencies
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach(sadFortuneService());
+	}
+}
