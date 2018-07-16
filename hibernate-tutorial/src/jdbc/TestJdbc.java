53b06ee711afdfdb35d146a7b97a22af43a985ae
Michaela Kubisova <kubiska18@gmail.com>
2018-07-16 14:07:42 +0200

    hibernate tutorial beginning

---
+++ hibernate-tutorial/src/jdbc/TestJdbc.java
@@ -0,0 +1,22 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		String jdbcUrl = "jdbs:mysql://127.0.0.1:3306/hb_student_tracker?useSSL=false";
+		String user = "hbstudent";
+		String password = "hbstudent";
+		try {
+			System.out.println("Connecting to db: " + jdbcUrl);
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, password);
+			System.out.println("Connected.");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
