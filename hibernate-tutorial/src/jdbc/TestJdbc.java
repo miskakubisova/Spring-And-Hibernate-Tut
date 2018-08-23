f1cd19f490c5a128b92b40729a5d05b78d8f9d94
Michaela Kubisova <kubiska18@gmail.com>
2018-08-23 11:32:54 +0200

    hibernate continue

--- hibernate-tutorial/src/jdbc/TestJdbc.java
+++ hibernate-tutorial/src/jdbc/TestJdbc.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class TestJdbc {
 
 	public static void main(String[] args) {
-		String jdbcUrl = "jdbs:mysql://127.0.0.1:3306/hb_student_tracker?useSSL=false";
+		String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/hb_student_tracker?useSSL=false";
 		String user = "hbstudent";
 		String password = "hbstudent";
 		try {
